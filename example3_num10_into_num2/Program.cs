dfe9e1ff93b5afc7fd010995388e8b7a05299948
Yaroslav.S <sysoev.yar@yandex.ru>
2022-04-14 20:36:19 +0500

    выполнил задачу 3 перевод числа из десятичной в двоичную систему

---
+++ example3_num10_into_num2/Program.cs
@@ -0,0 +1,52 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101             3  -> 11               2  -> 10
+
+Console.Clear();
+
+int Num = Promt("Введи число = ");
+Console.Write($"Число {Num} в двоичной системе -> ");
+
+int Len = 0;
+for (int number = Num; number > 0; Len++)
+{
+    number = number / 2;
+}
+
+int[] arr = new int[Len];
+for (int i = 0; i < Len; i++)
+{
+    arr[i] = Num % 2;
+    Num = Num / 2;
+}
+SwapArray(arr);
+PrintArray(arr);
+
+
+//--------------------------------------------
+
+int Promt(string messsage)
+{
+    Console.Write(messsage);
+    string strMassive = Console.ReadLine();
+    int answer = int.Parse(strMassive);
+    return answer;
+}
+
+void PrintArray(int[] collection)
+{
+    for (int index = 0; index <= collection.Length - 1; index++)
+    {
+        Console.Write($"{collection[index]}");
+    }
+}
+
+void SwapArray(int[] array)
+{
+    int temp = 0;
+    for (int index = 0; index < arr.Length / 2; index++)
+    {
+        temp = arr[index];
+        arr[index] = arr[arr.Length - 1 - index];
+        arr[arr.Length - 1 - index] = temp;
+    }
+}
